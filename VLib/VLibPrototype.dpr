60505c1c076b7ee7268ccb2de141fd997710c89e
ernikas <ernikas@gmail.com>
2013-02-18 20:07:15 +0000

    Prideti pervadinti failai

---
+++ VLib/VLibPrototype.dpr
@@ -0,0 +1,14 @@
+program VLibPrototype;
+
+uses
+  Vcl.Forms,
+  MainUNT in 'MainUNT.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
